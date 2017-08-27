5e2153b9cd0e8f972df6dd317798861ab63b498d
Tuomo H <tuomo.hopia@gmail.com>
2017-08-27 20:37:32 +0300

    asasf

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :video_chat, VideoChatWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [host: "https://atm-chat.appspot.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
