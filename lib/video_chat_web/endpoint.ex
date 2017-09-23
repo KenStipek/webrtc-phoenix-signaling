322c8eef35584e721b10b10dbd91e1aee68cd9fb
Tuomo H <tuomo.hopia@gmail.com>
2017-09-23 20:09:08 +0300

    hosting static files at elixir

--- lib/video_chat_web/endpoint.ex
+++ lib/video_chat_web/endpoint.ex
@@ -8,7 +8,8 @@ defmodule VideoChatWeb.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :video_chat, gzip: false,
+    # at: "/", from: :video_chat, gzip: false,
+    at: "/", from: "priv/static/react/static", gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
